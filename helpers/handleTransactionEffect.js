f20783061ddda47033b67b06000d03b9b092fffb
Charles Madu <charlesmadu001@gmail.com>
2025-06-27 14:40:29 +0100

    arrange codes

--- helpers/handleTransactionEffect.js
+++ helpers/handleTransactionEffect.js
@@ -8,33 +8,33 @@ const handleTransactionEffect = async ({ senderId, receiverId, type, amount }) =
   const balances = await updateBalance(senderId, receiverId, amount, type);
 
   if (type === 'transfer') {
-  const senderAccount = await Account.findByPk(senderId);
-  const receiverAccount = await Account.findByPk(receiverId);
-  const senderUserId = senderAccount?.user;
-  const receiverUserId = receiverAccount?.user;
+    const senderAccount = await Account.findByPk(senderId);
+    const receiverAccount = await Account.findByPk(receiverId);
+    const senderUserId = senderAccount?.user;
+    const receiverUserId = receiverAccount?.user;
 
-  const debitMsg = `₦${amount} has been debited from your account.`;
-  const creditMsg = `₦${amount} has been credited to your account.`;
+    const debitMsg = `₦${amount} has been debited from your account.`;
+    const creditMsg = `₦${amount} has been credited to your account.`;
 
-  notifications.push(await createTransactionNotification(senderUserId, debitMsg, 'debit'));
-  notifications.push(await createTransactionNotification(receiverUserId, creditMsg, 'credit'));
-}
+    notifications.push(await createTransactionNotification(senderUserId, debitMsg, 'debit'));
+    notifications.push(await createTransactionNotification(receiverUserId, creditMsg, 'credit'));
+  }
 
-if (type === 'deposit') {
-  const receiverAccount = await Account.findByPk(receiverId);
-  const receiverUserId = receiverAccount?.user;
+  if (type === 'deposit') {
+    const receiverAccount = await Account.findByPk(receiverId);
+    const receiverUserId = receiverAccount?.user;
 
-  const msg = `₦${amount} has been deposited to your account.`;
-  notifications.push(await createTransactionNotification(receiverUserId, msg, 'credit'));
-}
+    const msg = `₦${amount} has been deposited to your account.`;
+    notifications.push(await createTransactionNotification(receiverUserId, msg, 'credit'));
+  }
 
-if (type === 'withdrawal') {
-  const senderAccount = await Account.findByPk(senderId);
-  const senderUserId = senderAccount?.user;
+  if (type === 'withdrawal') {
+    const senderAccount = await Account.findByPk(senderId);
+    const senderUserId = senderAccount?.user;
 
-  const msg = `₦${amount} has been withdrawn from your account.`;
-  notifications.push(await createTransactionNotification(senderUserId, msg, 'debit'));
-}
+    const msg = `₦${amount} has been withdrawn from your account.`;
+    notifications.push(await createTransactionNotification(senderUserId, msg, 'debit'));
+  }
 
   return {
     notifications,
